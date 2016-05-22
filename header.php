df7f7aed7a8826de87e87bfae66c6ba5b48783e7
Jordan Neenan <jordan.neenan@gmail.com>
2016-05-23 09:52:35 +1000

    Some tidying of header.php

--- header.php
+++ header.php
@@ -1,13 +1,10 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title><?php wp_title(); ?> - <?php bloginfo( 'name' ); ?></title>
         <meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0,  minimum-scale=1.0, maximum-scale=1.0">
-
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
 
         <?php get_stylesheet_uri(); ?>
         <link rel='stylesheet' id='Main stylesheet-css'  href='<?php echo get_template_directory_uri(); ?>/css/style.css?v=1.0' type='text/css' />
@@ -20,7 +17,7 @@
         <!--[if lt IE 9]>
         <script src="http://html5shiv-printshiv.googlecode.com/svn/trunk/html5shiv-printshiv.js"></script>
         <![endif]-->
-        
+
     </head>
     <body>
         <?php wp_head(); ?>
@@ -40,5 +37,5 @@
                     <?php wp_nav_menu( array( 'theme_location' => 'main_nav', 'sort_column' => 'menu_order', 'container_class' => 'page-nav' ) ); ?>
                 </nav>
 
-     
+
             </header>
